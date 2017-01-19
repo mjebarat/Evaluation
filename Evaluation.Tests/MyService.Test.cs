0de53f04c2225bf223d11fb83b0808f34e7141e2
mosaab jebarat <mosaab.jebarat@gmail.com>
2017-01-19 16:48:02 +0100

    Evaluation
    tes

---
+++ Evaluation.Tests/MyService.Test.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evaluation;
+using Newtonsoft.Json.Linq;
+
+namespace Evaluation.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        #region Fibonacci Unit test
+        [TestMethod]
+        // Check if  Fibonacci(0) == 0
+        public void Check_Fibonacci_0()
+        {
+            var service = new MyService();
+
+            int number = service.Fibonacci(0);
+
+            Assert.AreEqual( number, 0);
+        }
+
+        [TestMethod]
+        // Check if  Fibonacci(101) == -1
+        public void Check_Fibonacci_101()
+        {
+            var service = new MyService();
+
+            int number = service.Fibonacci(101);
+
+            Assert.AreEqual(number, -1);
+        }
+
+        [TestMethod]
+        // Check if  Fibonacci(2) == 1
+        public void Check_Fibonacci_2()
+        {
+            var service = new MyService();
+
+            int number = service.Fibonacci(2);
+
+            Assert.AreEqual(number, 1);
+        }
+
+
+        [TestMethod]
+        // Check if  Fibonacci(5) == 5
+        public void Check_Fibonacci_5()
+        {
+            var service = new MyService();
+
+            int number = service.Fibonacci(5);
+
+            Assert.AreEqual(number, 5);
+        }
+
+        [TestMethod]
+        // Check if  Fibonacci(6) == 8
+        public void Check_Fibonacci_6()
+        {
+            var service = new MyService();
+
+            int number = service.Fibonacci(6);
+
+            Assert.AreEqual(number, 8);
+        }
+        #endregion
+
+        #region XmlToJson Unit Test
+        
+
+        [TestMethod]
+        // Check if  Fibonacci(0) == 0
+        public void Check_XmlToJson_XML_FORMAT_NOTWELL()
+        {
+            var service = new MyService();
+
+            string myJson = service.XmlToJson("<foo>hello</ba>");
+
+            Assert.AreEqual(myJson, "Bad XML Format");
+        }
+
+        [TestMethod]
+        public void Check_XmlToJson_SIMPLE_XML()
+        {
+            var service = new MyService();
+
+            string myJson = service.XmlToJson("<foo>bar</foo>");
+
+            // convert JSON to object
+            var resultJson = JObject.Parse(myJson);
+            var actualJson = JObject.Parse("{ \"foo\" : \"bar\" }");
+
+
+            Assert.IsTrue(JToken.DeepEquals(resultJson, actualJson));
+        }
+
+        [TestMethod]
+        public void Check_XmlToJson_LONG_XML()
+        {
+            var service = new MyService();
+
+            string myJson = service.XmlToJson("<TRANS><HPAY><lD>103</lD><STATUS>3</STATUS><EXTRA><lS3DS>0</lS3DS><AUTH>031183</AUTH></EXTRA><lNT_MSG /><MLABEL>501767XXXXXX6700</MLABEL><MTOKEN>projectOl</MTOKEN></HPAY></TRANS>");
+
+            // convert JSON to object
+            var resultJson = JObject.Parse(myJson);
+            var actualJson = JObject.Parse("{\"TRANS\":{\"HPAY\":{\"lD\":\"103\",\"STATUS\":\"3\",\"EXTRA\":{\"lS3DS\":\"0\",\"AUTH\":\"03'1183\"},\"MLABEL\":\"501767XXXXXX6700\",\"MTOKEN\":\"projectOl\"}}}");
+
+
+            Assert.IsTrue(JToken.DeepEquals(resultJson, actualJson));
+        }
+
+        #endregion
+    } 
+
+
+}
---
+++ Evaluation/MyService.asmx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Web.Services;
+using System.Xml;
+using System.Xml.Linq;
+using log4net;
+using log4net.Config;
+using Newtonsoft.Json;
+
+namespace Evaluation
+{
+    /// <summary>
+    /// Summary description for MyService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ScriptService]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MyService : System.Web.Services.WebService
+    {
+        protected readonly ILog _log = LogManager.GetLogger(typeof(MyService));
+
+        public MyService() 
+        {
+
+            XmlConfigurator.Configure();
+            _log = LogManager.GetLogger(typeof(MyService));
+            _log.Info("Service is Initialized ");
+       
+        }
+
+
+
+
+        [WebMethod]
+        public int Fibonacci(int number)
+        {
+            try
+            {
+                _log.Info("Fibonacci method start ");
+
+                if (number == 0)
+                    return 0;
+
+                if (number < 1 || number > 100)
+                    return -1;
+
+                if (number == 1)
+                    return 1;
+
+                _log.Info("Fibonacci method finished ");
+
+                return Fibonacci(number - 1) + Fibonacci(number - 2);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+                throw;
+            }
+        }
+
+        
+        //[WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)] // To get Json in response
+        public string XmlToJson(string xml)
+        {
+            try
+            {
+                _log.Info("XmlToJson method start ");
+
+                //var json = new JavaScriptSerializer().Serialize(ParseXml(XElement.Parse(xml)));
+                var json = JsonConvert.SerializeObject(ParseXml(XElement.Parse(xml)));
+
+                _log.Info("XmlToJson method finished ");
+
+                return json;
+            }
+
+            catch (XmlException ex)
+            {
+                _log.Info("Bad XML Format ");
+                return "Bad XML Format";
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+                throw;
+            }
+
+        }
+
+         private Dictionary<string, object> ParseXml(XElement xml)
+         {
+             var dict = new Dictionary<string, object>();
+
+             if (xml != null  && xml.HasElements)
+                 dict.Add(xml.Name.LocalName, xml.Elements()
+                                                    .SelectMany(e => ParseXml(e))
+                                                    .ToDictionary(x => x.Key, y => y.Value));
+             else if (!xml.IsEmpty)
+                 dict.Add(xml.Name.LocalName, xml.Value);
+
+             return dict; 
+         }
+
+
+
+
+       
+
+
+    }
+}
